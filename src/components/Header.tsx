11b54c1ee040da995f7015c2632b2e24e80f2759
Mohamed Abdelazeem <mohamedk.abdelazeem@gmail.com>
2024-04-09 03:57:47 +0200

    Add CartIcon component to Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import logo from '../assets/media/Mythic Games logo.png';
 import { Link } from 'react-router-dom';
+import CartIcon from './CartIcon';
 
 type HeaderProps = {
     isTransparent?: boolean;
@@ -9,11 +10,11 @@ export default function Header({isTransparent = true} : HeaderProps) {
     const background_color = isTransparent ? 'bg-transparent' : 'bg-secondary';
     return (
         <div className={`w-screen ${background_color} z-50`}>
-            <div className='w-full px-6 py-1 flex justify-between items-center'>
+            <div className='w-full px-10 py-1 flex justify-between items-center'>
                 <Link to='/'>
                     <img src={logo} alt='Mythic Games logo' className='md:w-20 w-16 h-auto hover:scale-110 transition-transform duration-500 ease-linear' />
                 </Link>
-                <p className='text-textColor'>Text Here</p>
+                <CartIcon size={0}/>
             </div>
         </div>
     )
